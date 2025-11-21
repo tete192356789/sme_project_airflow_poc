ad27cecd567093d1105cc7e41ca17d438abf700e
nachanon1923 <nachanon@sense-infotech.com>
2025-11-21 16:13:08 +0700

    wad

--- dags/mydag.py
+++ dags/mydag.py
@@ -1,8 +1,21 @@
+from datetime import datetime, timedelta
+
 from airflow.sdk import dag, task
-from pendulum import datetime
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
 
 
-@dag(start_date=datetime(2025, 4, 22), schedule="@daily", tags=["mydag"])
+@dag(
+    start_date=datetime(2024, 1, 1),
+    schedule=timedelta(seconds=30),
+    tags=["mydag"],
+    default_args=default_args,
+)
 def my_dag():
     @task()
     def my_dag_task(**context) -> str:
