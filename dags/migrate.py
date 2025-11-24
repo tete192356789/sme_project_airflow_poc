adf6bfbd746d495e1c0a46ea2ee29e5a6695c5c5
nachanon1923 <nachanon@sense-infotech.com>
2025-11-24 10:54:46 +0700

    FIXED : installed uv and pull update date

--- dags/migrate.py
+++ dags/migrate.py
@@ -3,26 +3,43 @@ from airflow.sdk import Asset, asset, dag, task
 
 
 @asset(schedule="@daily")
-def postgres_asset():
+def source_max_update_dt():
     hook = PostgresHook(postgres_conn_id="postgres_15")
-    records = hook.get_records("SELECT * FROM source_table;")
+    records = hook.get_records("SELECT MAX(updated_date) FROM source_table;")
     for rec in records:
         print(rec)
     return records
 
 
-@dag(schedule=[Asset("postgres_asset")])
+@asset(schedule=[Asset("source_max_update_dt")])
+def sink_max_update_dt():
+    hook = PostgresHook(postgres_conn_id="postgres_13")
+    records = hook.get_records("SELECT MAX(updated_date) FROM sink_table;")
+    for rec in records:
+        print(rec)
+    return records
+
+
+@dag(schedule=(Asset("source_max_update_dt") & Asset("sink_max_update_dt")))
 def after_postgres():
     @task
     def print_result(**context):
-        data = context["ti"].xcom_pull(
-            dag_id="postgres_asset",
-            task_ids="postgres_asset",
+        source_max_update_dt = context["ti"].xcom_pull(
+            dag_id="source_max_update_dt",
+            task_ids="source_max_update_dt",
             key="return_value",
             include_prior_dates=True,
         )
-        print("###################")
-        print(data[0])
+
+        sink_max_update_dt = context["ti"].xcom_pull(
+            dag_id="sink_max_update_dt",
+            task_ids="sink_max_update_dt",
+            key="return_value",
+            include_prior_dates=True,
+        )
+
+        print(f"SOURCE UPDATED DT: {source_max_update_dt}")
+        print(f"SINK UPDATED DT: {sink_max_update_dt}")
 
     print_result()
 
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from sme-project-airflow-poc!")
+
+
+if __name__ == "__main__":
+    main()
